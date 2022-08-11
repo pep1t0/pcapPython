459cf688ce4f89ae0d3b26714436156ac5df2e82
Pep1t0 <daniel.requena@aol.com>
2022-08-11 19:23:29 +0200

    v6

---
+++ exceptiones-ejemplos.py
@@ -0,0 +1,15 @@
+def readint(prompt, min, max):
+    
+    while True: 
+        try:
+            x = int(input())
+            assert x >= -10 and x <=10
+            return x
+        except ValueError:
+            print("Debes ingresar un valor entero")
+        except AssertionError:
+            print("Valores fuera de rango")
+    
+
+v = readint("Ingresa un numero de -10 a 10: ", -10,10)
+print('El numero es', v)
