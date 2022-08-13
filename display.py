d3b3b849e2743fc8f977006f26da8573909d2198
Pep1t0 <daniel.requena@aol.com>
2022-08-13 21:10:35 +0200

    display

--- display.py
+++ display.py
@@ -1,13 +1,27 @@
+'''
+SUBJECT:
+Seguramente has visto un display de siete segmentos.
+
+Es un dispositivo (a veces electrónico, a veces mecánico) diseñado para presentar un dígito decimal utilizando un subconjunto de siete segmentos.
+Si aún no sabes lo qué es, consulta la siguiente liga en Wikipedia artículo.
+
+Tu tarea es escribir un programa que puede simular el funcionamiento de un display de siete segmentos, aunque vas a usar LEDs individuales en lugar de segmentos.
+
+Tu código debe mostrar cualquier número entero no negativo ingresado por el usuario.
+'''
+
 def display(numero):
     
-    patrones = [['#', '###', '###', '# #', '###', '###', '###', '###', '###', '###'],
-                ['#', '  #', '  #', '# #', '#  ', '#  ', '  #', '# #', '# #', '# #'], 
-                ['#', '###', '###', '###', '###', '###', '  #', '###', '###', '# #'],
-                ['#', '#  ', '  #', '  #', '  #', '# #', '  #', '# #', '  #', '# #'], 
-                ['#', '###', '###', '  #', '###', '###', '  #', '###', '###', '###']]
+    patrones = [['  #', '###', '###', '# #', '###', '###', '###', '###', '###', '###'],
+                ['  #', '  #', '  #', '# #', '#  ', '#  ', '  #', '# #', '# #', '# #'], 
+                ['  #', '###', '###', '###', '###', '###', '  #', '###', '###', '# #'],
+                ['  #', '#  ', '  #', '  #', '  #', '# #', '  #', '# #', '  #', '# #'], 
+                ['  #', '###', '###', '  #', '###', '###', '  #', '###', '###', '###']]
     
     for j in range(5):            
         for i in str(numero):
             print(patrones[j][int(i)-1],end=' ')
         print(f"")
-display(23)
+
+display(123)
+display(9081726354)
