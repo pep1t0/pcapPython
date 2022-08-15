e172b7ba391d49e77d58d6030955af8b92e6298c
Pep1t0 <daniel.requena@aol.com>
2022-08-15 18:51:34 +0200

    Cifrado del Cesar

--- CifradoCesar.py
+++ CifradoCesar.py
@@ -1,14 +1,32 @@
 # Cifrado César
 
-text = input("Ingresa tu mensaje: ")
-cifrado = ''
-for char in text:
-    if not char.isalpha():
-        continue
-    char = char.upper()
-    code = ord(char) + 1
-    if code > ord('Z'):
-        code = ord('A')
-    cifrado += chr(code)
+def cifradoCesar(text):
+    
+    cifrado = ''
+    for char in text:
+        if not char.isalpha():
+            continue
+        char = char.upper()
+        code = ord(char) + 1
+        if code > ord('Z'):
+            code = ord('A')
+        cifrado += chr(code)
+    return cifrado
 
-print(cifrado)
+cifrar = input("Ingresa tu mensaje a cifrar: ")
+print(cifradoCesar(cifrar))
+
+def descrifradoCesar(cifrado):
+    text = ''
+    for char in cifrado:
+        if not char.isalpha():
+            continue
+        char = char.upper()
+        code = ord(char) - 1
+        if code < ord('A'):
+            code = ord('Z')
+        text += chr(code)
+    return text
+
+descifrar = input("Ingresa tu mensaje a descrifrar:")
+print(descrifradoCesar(descifrar))
